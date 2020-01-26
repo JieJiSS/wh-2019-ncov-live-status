bd50d8c23d02413d74862498f7df4796b02e4aa9
JieJiSS <c141028@protonmail.com>
2020-01-26 11:25:09 +0800

    update: anti-anti-spider

--- controller/_source.js
+++ controller/_source.js
@@ -13,7 +13,12 @@ async function sourceHTML() {
 
   let html;
   try {
-    html = await (fetch(CTX, {}).then(res => res.text()));
+    html = await (fetch(CTX, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+        "Cookie": "route=9305349c5ef09a0f85ffb86b4d77b5f5; JUTE_SESSION_ID=a379fee4-115e-4782-8ed4-964f60b9eb6a; DXY_USER_GROUP=93; JUTE_TOKEN=118797dd-8d56-4332-80a4-e83a010a210b; JSESSIONID=899D36324407C36BB74A0913CD20CAEA; route_bbs=4b7df70fe96acfe89c32b68456fd2ed3; JUTE_SESSION=575691fc08c0f464663f21a6a8ecfd97093fdad9e0ca37d45990903871863325213683c937b19fb6"
+      }
+    }).then(res => res.text()));
     cache.add("html", html, 5 * 60 * 1000);
   } catch (err) {
     if(err.name !== "FetchError") {
