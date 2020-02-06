5cf19809d0529d3f34aa6f4752fa2ab75806edb3
JieJiSS <c141028@protonmail.com>
2020-02-06 12:46:51 +0800

    chore: clean up dev codes

--- controller/_config.js
+++ controller/_config.js
@@ -1,9 +1,10 @@
 module.exports = {
   SOURCE_URL: "http://3g.dxy.cn/newh5/view/pneumonia",
 
-  HTML_CACHE_TTL: 6 * 1000, // 15 * 60 * 1000
-  PATIENTS_CACHE_TTL: 6 * 1000, // (14 * 60 + 10) * 1000,
-  NEWS_CACHE_TTL: 6 * 1000, // 14 * 60 * 1000,
+  HTML_CACHE_TTL: 15 * 60 * 1000,
+  PATIENTS_CACHE_TTL: (14 * 60 + 50) * 1000,
+  NEWS_CACHE_TTL: (14 * 60 + 40) * 1000,
   REQUEST_TIMEOUT: 15 * 1000,
 
-}
+  ENABLE_MANUAL_SOURCE: false,
+};
